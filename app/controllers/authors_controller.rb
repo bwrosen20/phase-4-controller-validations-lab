519f4821dd3b297f00d910db152d5cc36b6358b3
bwrosen20 <bwrosen20@gmail.com>
2023-06-09 11:11:20 -0400

    commit tree 9da22b1ac4595b872e2abf21d96d838229c9ddca

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,18 @@
 class AuthorsController < ApplicationController
   
+  
   def show
     author = Author.find(params[:id])
-
     render json: author
+  rescue ActiveRecord::RecordNotFound
+    render json: {error:author.errors.full_messages}, status: :not_found
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+      render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+      render json: {errors:invalid.record}, status: :unprocessable_entity
   end
 
   private
@@ -17,5 +20,6 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:update, :show]
   resources :authors, only: [:show, :create]
 end
